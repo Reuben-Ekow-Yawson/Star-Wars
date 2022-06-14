8e2da277230584c2d7576942655c7694dddd5c91
Reuben-Ekow-Yawson <87475624+Reuben-Ekow-Yawson@users.noreply.github.com>
2022-06-14 10:57:16 +0000

    "Optimize the code"

--- src/component/carousel/Carousel.tsx
+++ src/component/carousel/Carousel.tsx
@@ -1,21 +1,20 @@
 import React from "react";
-import { Carousel, Container } from "react-bootstrap";
+import { Carousel } from "react-bootstrap";
 import "./carousel.css";
 import Image from "react-bootstrap/Image";
 import { images } from "./CarouselImages";
 
-
 const CarouselComponent: React.FC = () => {
   return (
-    <Container className="carousel">
+    <div className="images">
       <Carousel fade>
         {images.map((img) => (
-          <Carousel.Item key={img.id} className='wrapper'>
+          <Carousel.Item key={img.id} className="wrapper">
             <Image src={img.image} alt="A-Family-at-War" className="starWar" />
           </Carousel.Item>
         ))}
       </Carousel>
-    </Container>
+    </div>
   );
 };
 
